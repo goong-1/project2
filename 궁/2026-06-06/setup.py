b887c9a9275ed584d857b4b983515bd85d2256e9
Byunggi.park <stonepbg@gmail.com>
2026-06-06 17:28:43 +0900

    Create setup.py

---
+++ 궁/2026-06-06/setup.py
@@ -0,0 +1,43 @@
+import os
+from glob import glob
+from setuptools import find_packages, setup
+
+package_name = 'p2_pkg'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    
+    # 파이썬 패키지 내부(src/p2_pkg/p2_pkg/)에 포함된 비-파이썬 파일(*.pt)을 자동으로 포함시킵니다.
+    # 이 방식은 python3.12 같은 경로를 하드코딩하지 않아도 되므로 훨씬 안전합니다.
+    package_data={
+        package_name: ['*.pt'],
+    },
+    
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*.launch.py'))),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='pbg',
+    maintainer_email='pbg@todo.todo',
+    description='라즈베리파이 4 하드웨어 최적화 및 모터·라이다 통합형 ROS 2 자율주행 패키지',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    
+    entry_points={
+        'console_scripts': [
+            'bridge_node = p2_pkg.bridge_node:main',
+            'brain_node = p2_pkg.brain_node:main',
+            'cam_line_node = p2_pkg.cam_line_node:main',
+            'cam_yolo_node = p2_pkg.cam_yolo_node:main',
+            'cam_image_node = p2_pkg.cam_image_node:main',
+            'subscriber = p2_pkg.subscriber:main',
+            'lidar_node = p2_pkg.lidar_node:main',
+        ],
+    },
+)
